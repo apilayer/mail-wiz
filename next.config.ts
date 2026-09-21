27970322ff22a4567d53022076ba02b6f19156ba
vaibhav sarkar <vaibhav.sarkar@apilayer.com>
2026-09-21 18:30:20 +0530

    workflow added to production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/devtools/verify-email',
+  assetPrefix: '/devtools/verify-email',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
